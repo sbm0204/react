9c7671f9e528517188f77c2eca551206cdfa3bf9
sbm0204 <song.bm24@gmail.com>
2025-09-04 17:05:35 +0900

    250904 conditional randering ~ component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer>
+      <h6>ⓒ Green Corp.</h6>
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <header>
+      <h1>듀공♥♡설아</h1>
+    </header>
+  )
+}
+
+export default Header;
